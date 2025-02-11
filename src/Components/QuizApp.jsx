8fe146be1180fdae6e65117d075a28c5711cbdcd
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-11 11:36:42 +0330

    feat : changeing question added

--- src/Components/QuizApp.jsx
+++ src/Components/QuizApp.jsx
@@ -5,40 +5,44 @@ export default function QuizApp() {
     const [Question,setQusertion]= useState([
         {
             questionText:"where is the capital of USA?",
+            id:1,
 
             answers:[
-                {answerText:"New Youk",isCorrect:false},
-                {answerText:"Paris",isCorrect:false},
-                {answerText:"LA",isCorrect:false},
-                {answerText:"Washington",isCorrect:true},
+                {answerText:"New Youk",isCorrect:false ,id:1},
+                {answerText:"Paris",isCorrect:false ,id:2},
+                {answerText:"LA",isCorrect:false ,id:3},
+                {answerText:"Washington",isCorrect:true ,id:4},
             ]
         },
         {
             questionText:"which of the following is used for web development?",
+            id:2,
 
             answers:[
-                {answerText:"JAva-script",isCorrect:true},
-                {answerText:"Go",isCorrect:false},
-                {answerText:"Dart",isCorrect:false},
-                {answerText:"C++",isCorrect:false},
+                {answerText:"JAva-script",isCorrect:true ,id:5},
+                {answerText:"Go",isCorrect:false ,id:6},
+                {answerText:"Dart",isCorrect:false ,id:7},
+                {answerText:"C++",isCorrect:false ,id:8} ,
             ]
         },  {
             questionText:"which of the following is a popular java-script library for user inter face?",
+            id:3,
 
             answers:[
-                {answerText:"Angular",isCorrect:false},
-                {answerText:"Vue.js",isCorrect:false},
-                {answerText:"React",isCorrect:true},
-                {answerText:"Ember",isCorrect:false},
+                {answerText:"Angular",isCorrect:false ,id:9},
+                {answerText:"Vue.js",isCorrect:false ,id:10},
+                {answerText:"React",isCorrect:true ,id:11},
+                {answerText:"Ember",isCorrect:false ,id:12},
             ]
         },  {
             questionText:"what is the chemical symbol for water?",
+            id:4,
 
             answers:[
-                {answerText:"CO2",isCorrect:false},
-                {answerText:"H2O",isCorrect:true},
-                {answerText:"NaCl",isCorrect:false},
-                {answerText:"Fe",isCorrect:false},
+                {answerText:"CO2",isCorrect:false, id:13},
+                {answerText:"H2O",isCorrect:true, id:14},
+                {answerText:"NaCl",isCorrect:false, id:15},
+                {answerText:"Fe",isCorrect:false, id:16},
             ]
         },
     ])
@@ -58,6 +62,10 @@ export default function QuizApp() {
             })
         }
 
+        setCurrentQuestion(prev=>{
+            return prev+1
+        })
+
     }
 
 
@@ -75,7 +83,7 @@ export default function QuizApp() {
     {Question[currentQuestion].answers.map(answer=>{
 
         
-    return <button onClick={()=>{clickHandler(answer.isCorrect)}} className="hover:bg-blue-700 p-1 border rounded-md w-36">{answer.answerText}</button>
+    return <button key={answer.id} onClick={()=>{clickHandler(answer.isCorrect)}} className="hover:bg-blue-700 p-1 border rounded-md w-36">{answer.answerText}</button>
     })}
        
  
