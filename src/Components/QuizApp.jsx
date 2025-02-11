389e66e7ccc6df5a35c4b614c8dd0b129d4ecd91
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-11 11:07:41 +0330

    feat : qustion state created

--- src/Components/QuizApp.jsx
+++ src/Components/QuizApp.jsx
@@ -1,5 +1,50 @@
+import { useState } from "react"
 
 export default function QuizApp() {
+
+    const [Question,setQusertion]= useState([
+        {
+            questionText:"wwhere is the capital of USA?",
+
+            answers:[
+                {answerText:"New Youk",isCorrect:false},
+                {answerText:"Paris",isCorrect:false},
+                {answerText:"LA",isCorrect:false},
+                {answerText:"Washington",isCorrect:true},
+            ]
+        },
+        {
+            questionText:"which of the following is used for web development?",
+
+            answers:[
+                {answerText:"JAva-script",isCorrect:true},
+                {answerText:"Go",isCorrect:false},
+                {answerText:"Dart",isCorrect:false},
+                {answerText:"C++",isCorrect:false},
+            ]
+        },  {
+            questionText:"which of the following is a popular java-script library for user inter face?",
+
+            answers:[
+                {answerText:"Angular",isCorrect:false},
+                {answerText:"Vue.js",isCorrect:false},
+                {answerText:"React",isCorrect:true},
+                {answerText:"Ember",isCorrect:false},
+            ]
+        },  {
+            questionText:"what is the chemical symbol for water?",
+
+            answers:[
+                {answerText:"CO2",isCorrect:false},
+                {answerText:"H2O",isCorrect:true},
+                {answerText:"NaCl",isCorrect:false},
+                {answerText:"Fe",isCorrect:false},
+            ]
+        },
+    ])
+
+
+
   return (
     <div className="shadow-2xl Quiz_app rounded-xl mx-auto mt-24 p-5 w-[430px] h-[220px] bg-blue-900 flex gap-10 justify-center">
 
