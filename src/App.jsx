f69954abfc271436ea6cd690c9c885eed0202a34
sahand mahdilu <sahand.mahdilu29@gmail.com>
2025-02-11 10:19:09 +0330

    feat : quizApp component created

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import QuizApp from './Components/QuizApp'
 
 function App() {
 
   return (
     <>
-     <h1 className='bg-red-500'>hello</h1>
+    <QuizApp/>
     </>
   )
 }
---
+++ src/Components/QuizApp.jsx
@@ -0,0 +1,6 @@
+
+export default function QuizApp() {
+  return (
+    <div>QuizApp</div>
+  )
+}
